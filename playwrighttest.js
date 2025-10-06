03b31549b4565bb714b56c296d9f60b949af1d22
nicksakwa <nsakwa@gmail.com>
2025-10-06 23:53:35 +0300

    take screenshot test with playwright

--- playwrighttest.js
+++ playwrighttest.js
@@ -7,3 +7,10 @@ test('Verify page title and content', async({ page })=>{
     await expect(heading).toBeVisible();
     await expect(heading).tohaveText('Example domain');
 });
+
+test('Take a screenshot of the main page' , async({ page })=>{
+    await page.goto('https://playwright.dev/');
+    await page.screeshot({ path: 'test-results/playwright-homepage.png', fullpage: true });
+    console.log('Screenshot saved to test-results/playwright-homepage.png');
+    await expect(page).toHaveURL(/.*playwright.dev/);
+})
